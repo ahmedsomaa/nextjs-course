ea266ca2671b318f840dc785f5b0dfee8a15d037
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-01-19 17:48:08 +0200

    add link component navigation example

--- routing/pages/index.js
+++ routing/pages/index.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog" legacyBehavior>
+        <a>Blog</a>
+      </Link>
+      <br />
+      <Link href="/product" legacyBehavior>
+        <a>Products</a>
+      </Link>
+    </div>
+  );
 }
--- routing/pages/product/index.js
+++ routing/pages/product/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function ProductList() {
   return (
-    <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-    </>
+    <div>
+      <Link legacyBehavior href="/product/1">
+        <a>Product 1</a>
+      </Link>
+      <Link legacyBehavior href="/product/2">
+        <a>Product 2</a>
+      </Link>
+      <Link legacyBehavior href="/product/3">
+        <a>Product 3</a>
+      </Link>
+      <Link legacyBehavior href="/">
+        <a>Home</a>
+      </Link>
+    </div>
   );
 }
