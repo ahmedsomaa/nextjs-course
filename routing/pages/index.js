b09ded4e10db98346ae132ba73e285aedccedf89
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-01-19 17:52:52 +0200

    add navigating programmatically example

--- routing/pages/index.js
+++ routing/pages/index.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => router.push("/product");
+
   return (
     <div>
       <h1>Home Page</h1>
@@ -11,6 +16,7 @@ export default function Home() {
       <Link href="/product" legacyBehavior>
         <a>Products</a>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
