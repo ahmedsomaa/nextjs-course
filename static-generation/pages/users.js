12da0c099af375344dc25ff75f4205e39193cc8f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-01-23 17:16:51 +0200

    add static generation example

---
+++ static-generation/pages/users.js
@@ -0,0 +1,24 @@
+export default function UsersList({ users }) {
+  return (
+    <div>
+      <h1>List of users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
