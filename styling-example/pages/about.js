4249cb9825d498a3b6ae8fdedc5419bafdbb32e9
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-02-29 16:18:01 +0200

    add sass support

--- styling-example/pages/about.js
+++ styling-example/pages/about.js
@@ -1,4 +1,4 @@
-import styles from "@/styles/About.module.css";
+import styles from "@/styles/About.module.scss";
 
 export default function About() {
   return <h2 className={styles.highlight}>About</h2>;
